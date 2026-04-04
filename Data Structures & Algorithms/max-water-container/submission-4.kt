eff29d069bdc8815722c6a85b3588c55c6e3083d
Sagar-Bhalsing <77963069+Sagar-Bhalsing@users.noreply.github.com>
2026-04-04 22:35:14 +0530

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun maxArea(heights: IntArray): Int {
+        var max = 0
+        var l = 0
+        var r = heights.size-1
+        while(l<r){
+            if(heights[l]>heights[r]){
+                max = max(max,heights[r] * (r-l))
+                r -= 1
+            }
+            else{
+                max = max(max,heights[l] * (r-l))
+                l+=1
+            }
+        }
+        return max
+    }
+   
+}
